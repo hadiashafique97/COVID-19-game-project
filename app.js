09dd93681d179e9183aff7e3b11821160055c264
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2022-11-08 16:30:38 -0600

    scoreboard updated

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("ello world")
+
+// need to create a function for running the game
+// create a function for checking winner 
+// create variables 
+// create a function ,random selection for the computer 
+//create functions that when clickd it matches the picture 
+// define score and then change at the end 
+//add a p tag that says make your move 
