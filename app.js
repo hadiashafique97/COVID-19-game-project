81950f1b8177a770755a6de9d3aa0e138ad61e40
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2022-11-09 09:30:50 -0600

    initialized my variables

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-console.log("ello world")
-
+const newGameButton = document.querySelector('.new-game')
+const human = document.querySelector('#human')
+const vaccine = document.querySelector('#vaccine')
+const virus = document.querySelector('#virus')
+const result = document.querySelector('#result')
+let myPlayerScore = document.querySelector('#myplayer-score')
+let theComputerScore = document.querySelector('#enemy-score')
 // need to create a function for running the game
 // create a function for checking winner 
 // create variables 
@@ -7,3 +12,12 @@ console.log("ello world")
 //create functions that when clickd it matches the picture 
 // define score and then change at the end 
 //add a p tag that says make your move 
+
+function startANewGame(){
+newGameButton.addEventListener('click',gameRound())
+}
+
+
+function gameRound(){
+
+}
